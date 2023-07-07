8cf1382ce097d0dfb6cf0110ba1b439a5c591642
khsrla9806 <kimhunsope@naver.com>
2023-07-07 13:35:23 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/board/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
