4f58784e70a6d07e0e7ae1913346047db2db42fe
khsrla9806 <kimhunsope@naver.com>
2023-07-10 17:54:56 +0900

    [#3] feat: 게시글 생성 요청 시 유효성 검사 추가
    - title은 필수 입력 값, 빈 문자, 공백 문자 허용하지 않음. 길이는 최대 255자
    - content는 필수 입력 값, 빈 문자, 공백 문자 허용하지 않음. 길이는 최대 20000자

--- src/main/java/com/board/board/dto/BoardDto.java
+++ src/main/java/com/board/board/dto/BoardDto.java
@@ -2,13 +2,21 @@ package com.board.board.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 
 public class BoardDto {
 
     @Getter
     @Setter
     public static class CreateRequest {
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
+        @Length(max = 255, message = "제목은 255자를 넘어갈 수 없습니다.")
         private String title;
+
+        @NotBlank(message = "내용은 필수 입력 값입니다.")
+        @Length(max = 20000, message = "본문은 20,000자를 넘어갈 수 없습니다.")
         private String content;
     }
 
