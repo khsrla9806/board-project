05c8c36edae86f7011c4f864e187cdcf9cbfac3d
khsrla9806 <kimhunsope@naver.com>
2023-07-10 17:05:20 +0900

    [#3] feat: 수정, 조회에 필요한 Board Dto 클래스 생성
    - Service 인터페이스 추상화를 위해서 생성해뒀습니다. 추후 수정 가능성 있습니다.

--- src/main/java/com/board/board/dto/BoardDto.java
+++ src/main/java/com/board/board/dto/BoardDto.java
@@ -11,4 +11,34 @@ public class BoardDto {
         private String title;
         private String content;
     }
+
+    @Getter
+    @Setter
+    public static class UpdateRequest {
+        private Long id;
+        private String title;
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    public static class ListResponse {
+        private Long id;
+        private String title;
+        private String content;
+        private String thumbnail;
+        private String nickname;
+    }
+
+    @Getter
+    @Setter
+    public static class DetailResponse {
+        private Long id;
+        private String title;
+        private String content;
+        private String thumbnail;
+
+        // TODO: private Member member;
+        // TODO: private List<Comment> comment;
+    }
 }
