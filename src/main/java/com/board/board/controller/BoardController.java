677dff1114c03ece52b8d3b79a1461c113ea2309
khsrla9806 <kimhunsope@naver.com>
2023-07-11 17:39:05 +0900

    [#7] feat: 카테고리별로 요청을 분기하는 목록 조회 Controller 메서드 구현
    - 카테고리는 PathVariable로 받아서 내부에서 요청을 분기했습니다.
    - 하드 코딩된 부분은 추후 상수화 리펙토링 작업이 필요할 것으로 보입니다.
    - Pageable 객체를 받고, default size는 6으로 하고, id 값을 가지고 내림차순 정렬을 진행했습니다.
    - 입력된 카테고리가 common, pro가 아닌 경우 Exception을 발생합니다. 추후 Custom Exception으로 변경

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.board.board.controller;
 import com.board.board.dto.BoardDto;
 import com.board.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+
+import static com.board.board.type.Category.COMMON;
+import static com.board.board.type.Category.PRO;
 
 @RequiredArgsConstructor
 @RequestMapping("/board")
@@ -38,5 +45,30 @@ public class BoardController {
         Long boardId = boardService.save(dto, thumbnail);
 
         return "redirect:/board/" + boardId;
+    }
+
+    @GetMapping("/{category}")
+    public String boardList(
+            @PathVariable String category,
+            Model model,
+            @PageableDefault(size = 6, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<BoardDto.ListResponse> boards;
+
+        // TODO: 상수화
+        if (category.equals("common")) {
+            boards = boardService.findAllByCategory(COMMON, pageable);
+            model.addAttribute("categoryTitle", "새싹 회원");
+        } else if (category.equals("pro")) {
+            boards = boardService.findAllByCategory(PRO, pageable);
+            model.addAttribute("categoryTitle", "우수 회원");
+        } else {
+            throw new RuntimeException(); // TODO: 커스텀 Exception 처리
+        }
+        model.addAttribute("boards", boards);
+        model.addAttribute("category", category);
+        model.addAttribute("maxPage", 5);
+
+        return "board/boards";
     }
 }
