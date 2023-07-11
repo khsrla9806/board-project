41aa1008c96788f3049b30a422456c47c96fe006
khsrla9806 <kimhunsope@naver.com>
2023-07-11 17:43:44 +0900

    [#7] feat: BoardService 구현체의 findAllByCategory 메서드 기능 구현
    - BoardRepository의 쿼리를 통해서 Page<Board>를 받도록 했습니다.
    - Page<Board>를 map()을 사용해서 Page<BoardDto.ListResponse>로 변환하여 반환

--- src/main/java/com/board/board/service/impl/BoardServiceImpl.java
+++ src/main/java/com/board/board/service/impl/BoardServiceImpl.java
@@ -6,13 +6,14 @@ import com.board.board.repository.BoardRepository;
 import com.board.board.service.BoardService;
 import com.board.board.type.Category;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 import static com.board.board.type.Status.ACTIVE;
@@ -61,8 +62,10 @@ public class BoardServiceImpl implements BoardService {
         return homeDir + File.separator + uploadFolderName + File.separator + uuid + thumbnailOriginalName;
     }
 
-    public List<BoardDto.ListResponse> findAllByCategory(Category category) {
-        return null;
+    public Page<BoardDto.ListResponse> findAllByCategory(Category category, Pageable pageable) {
+        Page<Board> boards = boardRepository.findAllByCategoryAndStatus(category, ACTIVE, pageable);
+
+        return boards.map(BoardDto.ListResponse::fromEntity);
     }
 
     public BoardDto.DetailResponse findById(Long id) {
