47828ce7482bc49f29b191e92f03238cbfba5ce6
khsrla9806 <kimhunsope@naver.com>
2023-07-11 17:39:59 +0900

    [#7] refactor: BoardService 인터페이스의 findAllByCategory 메서드의 반환 타입 변경
    - 기존 List 반환 타입에서 Page 반환 타입으로 변경했습니다.

--- src/main/java/com/board/board/service/BoardService.java
+++ src/main/java/com/board/board/service/BoardService.java
@@ -2,13 +2,13 @@ package com.board.board.service;
 
 import com.board.board.dto.BoardDto;
 import com.board.board.type.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 public interface BoardService {
     Long save(BoardDto.CreateRequest dto, MultipartFile thumbnail);
-    List<BoardDto.ListResponse> findAllByCategory(Category category); // TODO: Pageable
+    Page<BoardDto.ListResponse> findAllByCategory(Category category, Pageable pageable);
     BoardDto.DetailResponse findById(Long id);
     Long update(BoardDto.UpdateRequest dto, MultipartFile thumbnail);
     void deleteById(Long id);
