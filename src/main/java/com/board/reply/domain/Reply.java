68be116c893123b179e9250874c0f44c46099b34
SYH <syh05247@naver.com>
2023-07-14 01:15:14 +0900

    [#8] feat: 대댓글 과 게시글 연관관계 설정
    * 부모 댓글은 @ManyToOne 자식 댓글은 @OneToMany 로 설정
    * Board 와 @ManyToOne 로 설정

--- src/main/java/com/board/reply/domain/Reply.java
+++ src/main/java/com/board/reply/domain/Reply.java
@@ -1,16 +1,24 @@
 package com.board.reply.domain;
 
+import com.board.board.domain.Board;
 import lombok.*;
 import org.hibernate.Hibernate;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
+@ToString
 public class Reply {
 
     @Id
@@ -20,9 +28,25 @@ public class Reply {
     @Column(nullable = false, length = 50)
     private String content;
 
-    private Long boardId;       // TODO : Board 와 연관관계 설정
+    // null 일 경우 최상위 댓글
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    @Setter
+    private Reply parent;
+
+    // 자식 댓글들
+    @ToString.Exclude
+    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    private List<Reply> children = new ArrayList<>();
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
 
     private Long memberId;      // TODO: MEMBER 와 연관관계 설정
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 
     @Override
     public boolean equals(Object o) {
