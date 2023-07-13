61ec10c984936f557031f49aba3ba6dcc76af728
SYH <syh05247@naver.com>
2023-07-14 01:30:26 +0900

    [#8] feat: ReplyController ListBoardAndReply 추가
    * 게시글과 댓글을 함께 출력하는 controller 로직 추가
    * ModelMap 형태로 service 로직으로 가져온 BoardWithReplyDto 타입을 model 에 각 각 설정하였음

--- src/main/java/com/board/reply/controller/ReplyController.java
+++ src/main/java/com/board/reply/controller/ReplyController.java
@@ -4,18 +4,28 @@ import com.board.reply.dto.ReplyDto;
 import com.board.reply.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/replies")
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class ReplyController {
 
     private final ReplyService replyService;
 
-    @PostMapping("/new")
+    @GetMapping("/{boardId}")
+    public String ListBoardAndReply(@PathVariable Long boardId, ModelMap model) {
+        ReplyDto.BoardWithReplyDto boardWithReplies = replyService.findBoardWithReplies(boardId);
+
+        model.addAttribute("board", boardWithReplies);
+        model.addAttribute("replies", boardWithReplies.getReplyResponseDtos());
+
+        return "/replies/boardAndReply";
+
+    }
+
+    @PostMapping("/reply/new")
     public String addReply(ReplyDto.ReplyRequestDto replyRequestDto) {
         replyService.addReply(replyRequestDto);
 
