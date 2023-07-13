ba4450ac5d8fe91821c71a02f416e873f2cd6677
SYH <syh05247@naver.com>
2023-07-14 01:22:58 +0900

    [#8] feat: ReplyService 로직 수정 과 Dto 추가
    * ReplyServiceImpl 를 impl 폴더로 이동
    * ReplyService interface 에 findBoardWithReplies 로직을 추가
    * findBoardWithReplies 로직은 게시글과 댓글의 유효성 검사를 한 후 Dto 로 출력시킨다.
    * addReply 에 유효성 검사를 추가함
    * toEntity 에 부모 댓글 (parenReply) 를 파라미터로 넣는 형식으로 변경
    * 게시글과 댓글을 함께 출력시키는 BoardWithReplyDto 추가

--- src/main/java/com/board/reply/dto/ReplyDto.java
+++ src/main/java/com/board/reply/dto/ReplyDto.java
@@ -1,24 +1,28 @@
 package com.board.reply.dto;
 
+import com.board.board.domain.Board;
 import com.board.reply.domain.Reply;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReplyDto {
 
     @Getter
+    @Setter
     @AllArgsConstructor
+    @ToString
     @Builder
     public static class ReplyRequestDto {
 
-        private Long id;
-
         @NotNull
-        private Long boardId;       // TODO : Board 와 연관관계 설정
+        private Long boardId;
+
+        private Long parentId;
 
         @NotNull
         private Long memberId;      // TODO : Member 와 연관관계 설정
@@ -26,11 +30,12 @@ public class ReplyDto {
         @NotBlank
         private String content;
 
-        public Reply toEntity() {
+        public Reply toEntity(Board board, Reply parenReply, Long userId) {
             return Reply.builder()
-                    .id(id)
-                    .boardId(boardId)
                     .content(content)
+                    .parent(parenReply)
+                    .board(board)
+                    .memberId(userId)
                     .build();
         }
     }
@@ -38,11 +43,52 @@ public class ReplyDto {
     @Getter
     @AllArgsConstructor
     @Builder
+    @ToString
     public static class ReplyResponseDto {
         private Long id;
         private String content;
+        private Long parentId;
         private Long boardId;
         private Long memberId;
+        private List<ReplyResponseDto> children;
+
+        public static ReplyResponseDto fromEntity(Reply reply) {
+            List<ReplyResponseDto> children = new ArrayList<>();
+
+            if (reply.getChildren() != null) {
+                for (Reply child : reply.getChildren()) {
+                    children.add(fromEntity(child));
+                }
+            }
+
+            return ReplyResponseDto.builder()
+                    .id(reply.getId())
+                    .content(reply.getContent())
+                    .parentId(
+                            reply.getParent() != null ? reply.getParent().getId() : null
+                    )
+                    .boardId(reply.getBoard().getId())
+                    .memberId(reply.getMemberId())
+                    .children(children)
+                    .build();
+
+        }
+
     }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class BoardWithReplyDto {
+        private Long id;
+        private Long memberId;
+        private LocalDateTime createdAt;
+        private String title;
+        private String content;
+        private String thumbnail;
+        private List<ReplyResponseDto> replyResponseDtos;
+
+    }
+
 
 }
---
+++ src/main/java/com/board/reply/service/impl/ReplyServiceImpl.java
@@ -0,0 +1,76 @@
+package com.board.reply.service.impl;
+
+import com.board.board.domain.Board;
+import com.board.board.repository.BoardRepository;
+import com.board.reply.domain.Reply;
+import com.board.reply.dto.ReplyDto;
+import com.board.reply.repository.ReplyRepository;
+import com.board.reply.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReplyServiceImpl implements ReplyService {
+
+    private final BoardRepository boardRepository;
+
+    private final ReplyRepository replyRepository;
+
+    // 특정 게시글의 댓글 출력
+    @Override
+    public ReplyDto.BoardWithReplyDto findBoardWithReplies(Long boardId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다 : " + boardId));
+
+        List<Reply> replies = replyRepository.findByBoardIdAndParentIsNull(board.getId());
+
+        List<ReplyDto.ReplyResponseDto> replyResponseDtos = replies.stream()
+                .map(ReplyDto.ReplyResponseDto::fromEntity)
+                .collect(Collectors.toList());
+
+        return ReplyDto.BoardWithReplyDto.builder()
+                .id(board.getId())
+                .memberId(board.getMember_id())
+                .createdAt(board.getCreatedAt())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .thumbnail(board.getThumbnail())
+                .replyResponseDtos(replyResponseDtos)
+                .build();
+    }
+
+    // 댓글 등록
+    @Override
+    public void addReply(ReplyDto.ReplyRequestDto replyRequestDto) {
+
+        Board board = boardRepository.findById(replyRequestDto.getBoardId())
+                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다 : " + replyRequestDto.getBoardId()));
+
+        Reply parenReply = null;
+        if (replyRequestDto.getParentId() != null) {
+            parenReply = replyRepository.findById(replyRequestDto.getParentId())
+                    .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다. : " + replyRequestDto.getParentId()));
+        }
+
+        Long userId = 1L;   // TODO : Member 와 연관관계 설정 현재는 임시로 1L 값을 넣기로 함
+
+        Reply reply = replyRequestDto.toEntity(board, parenReply, userId);
+
+        replyRepository.save(reply);
+    }
+
+    // 댓글 삭제
+    @Override
+    public void removeReply(Long id) {
+
+        replyRepository.deleteById(id);
+
+    }
+}
