53271f7628c277a7f67d3de7ed79aa378f050f8c
khsrla9806 <kimhunsope@naver.com>
2023-07-13 22:20:46 +0900

    [#16] feat: 게시글 검색 Repository 메서드를 JPQL을 사용하여 구현
    - Member와 Joing하여 nickname도 검색 범위에 두어야하기 때문에 @Query에 JPQL을 사용
    - 기존에 목록 가져올 때 사용하던 Pagable 객체를 그대로 사용하여 Page 객체를 얻을 수 있도록 설정

--- src/main/java/com/board/board/repository/BoardRepository.java
+++ src/main/java/com/board/board/repository/BoardRepository.java
@@ -6,8 +6,33 @@ import com.board.board.type.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<Board> findAllByCategoryAndStatus(Category category, Status status, Pageable pageable);
+
+    @Query(
+            "SELECT b " +
+            "FROM Board b " +
+            "LEFT JOIN b.member m " +
+            "WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword% OR m.nickname LIKE %:keyword%) " +
+            "AND b.category = :category AND b.status = :status"
+    )
+    Page<Board> findAllByCategoryAndKeyword(
+            @Param("category") Category category,
+            @Param("status") Status status,
+            @Param("keyword") String keyword,
+            Pageable pageable);
+
     Page<Board> findAllByStatus(Status status, Pageable pageable);
+
+    @Query(
+            "SELECT b " +
+            "FROM Board b " +
+            "LEFT JOIN b.member m " +
+            "WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword% OR m.nickname LIKE %:keyword%) " +
+            "AND b.status = :status"
+    )
+    Page<Board> findAllByKeyword(@Param("status") Status status, @Param("keyword") String keyword, Pageable pageable);
 }
