e71028086d060e0622b1154d96d0239ddc706cec
SYH <syh05247@naver.com>
2023-07-14 01:22:58 +0900

    [#8] feat: ReplyService 로직 수정 과 Dto 추가
    * ReplyServiceImpl 를 impl 폴더로 이동
    * ReplyService interface 에 findBoardWithReplies 로직을 추가
    * findBoardWithReplies 로직은 게시글과 댓글의 유효성 검사를 한 후 Dto 로 출력시킨다.
    * addReply 에 유효성 검사를 추가함
    * toEntity 에 부모 댓글 (parenReply) 를 파라미터로 넣는 형식으로 변경
    * 게시글과 댓글을 함께 출력시키는 BoardWithReplyDto 추가

--- src/main/java/com/board/reply/service/ReplyService.java
+++ src/main/java/com/board/reply/service/ReplyService.java
@@ -4,6 +4,8 @@ import com.board.reply.dto.ReplyDto;
 
 public interface ReplyService {
 
+    ReplyDto.BoardWithReplyDto findBoardWithReplies(Long boardId);
+
     void addReply(ReplyDto.ReplyRequestDto replyRequestDto);
 
     void removeReply(Long id);
