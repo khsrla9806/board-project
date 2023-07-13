91fe3734c0b3699879b8dd5d9e29bee06ff92e80
SYH <syh05247@naver.com>
2023-07-14 01:25:02 +0900

    [#8] feat: ReplyRepository 로직 추가
    * findByBoardIdAndParentIsNull 를 추가 댓글이 null 상태인 경우는 부모댓글이기 때문에 부모 댓글만 출력시키고 그에 따라 자식댓글들이 중복되지 않는 쿼리문을 작성

--- src/main/java/com/board/reply/repository/ReplyRepository.java
+++ src/main/java/com/board/reply/repository/ReplyRepository.java
@@ -3,6 +3,10 @@ package com.board.reply.repository;
 import com.board.reply.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findByBoardIdAndParentIsNull(Long boardId);
 
 }
