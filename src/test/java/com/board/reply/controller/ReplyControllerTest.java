cabf1650a60a2c4de511d54a3a92af8e5c705b2d
SYH <syh05247@naver.com>
2023-07-14 13:58:16 +0900

    [#8] test: Reply test 코드 수정
    * Reply 도메인의 연관관계 및 Dto 수정에 따란 test 코드도 그에 맞게 수정하였다.
    * Test 는 모두 성공하였다.
    * 추가로 Member 의 연관관계를 설정하면 테스트 코드도 다시 변경해야함

--- src/test/java/com/board/reply/controller/ReplyControllerTest.java
+++ src/test/java/com/board/reply/controller/ReplyControllerTest.java
@@ -1,71 +1,77 @@
 package com.board.reply.controller;
 
+import com.board.exception.GlobalExceptionHandler;
 import com.board.reply.dto.ReplyDto;
-import com.board.reply.service.ReplyServiceImpl;
+import com.board.reply.service.ReplyService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @DisplayName("ReplyController 테스트")
 @WebMvcTest(
-        controllers = ReplyController.class,
-        excludeAutoConfiguration = SecurityAutoConfiguration.class
+        controllers = ReplyController.class
 )
 class ReplyControllerTest {
 
     private final MockMvc mvc;
 
     @MockBean
-    private ReplyServiceImpl replyServiceImpl;
+    private GlobalExceptionHandler globalExceptionHandler;
 
-    public ReplyControllerTest(@Autowired MockMvc mvc, @Autowired ReplyServiceImpl replyServiceImpl) {
+    @MockBean
+    private ReplyService replyService;
+
+    ReplyControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
-        this.replyServiceImpl = replyServiceImpl;
     }
 
     @DisplayName("댓글 등록 - 정상 호출")
+    @WithMockUser
     @Test
     public void replyList() throws Exception {
-        //given
-        final Long boardId = 1L;
-        ReplyDto.ReplyRequestDto.builder()
-                .content("댓글 컨트롤러 테스트 작성 중")
-                .memberId(1L)
-                .boardId(boardId)
-                .build();
-        willDoNothing().given(replyServiceImpl).addReply(any(ReplyDto.ReplyRequestDto.class));
+        // given
+        willDoNothing().given(replyService).addReply(
+                ReplyDto.ReplyRequestDto.builder()
+                        .boardId(1L)
+                        .parentId(null)
+                        .memberId(1L)
+                        .content("댓글 테스트입니다.")
+                        .build()
+        );
 
-        //when & then
-        mvc.perform(
-                        post("/replies/new")
+        // when & then
+        mvc.perform(post("/board/reply/new")
+                .with(csrf())
                 )
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
-        then(replyServiceImpl).should().addReply(any(ReplyDto.ReplyRequestDto.class));
+        then(replyService).should().addReply(any(ReplyDto.ReplyRequestDto.class));
     }
 
     @DisplayName("댓글 삭제 - 정상 호출")
+    @WithMockUser
     @Test
     public void deleteReply() throws Exception {
         //given
         final Long replyId = 1L;
-        willDoNothing().given(replyServiceImpl).removeReply(replyId);
+        willDoNothing().given(replyService).removeReply(replyId);
 
         //when & then
-        mvc.perform(
-                        post("/replies/" + replyId + "/delete")
+        mvc.perform(post("/board/reply/delete/" + replyId)
+                        .with(csrf())
                 )
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
-        then(replyServiceImpl).should().removeReply(replyId);
+        then(replyService).should().removeReply(replyId);
     }
 
 }
--- src/test/java/com/board/reply/repository/ReplyRepositoryTest.java
+++ src/test/java/com/board/reply/repository/ReplyRepositoryTest.java
@@ -1,6 +1,12 @@
 package com.board.reply.repository;
 
+import com.board.board.domain.Board;
+import com.board.board.repository.BoardRepository;
+import com.board.board.type.Category;
+import com.board.board.type.Status;
 import com.board.reply.domain.Reply;
+import com.board.reply.dto.ReplyDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,32 +20,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ReplyRepositoryTest {
 
+    private final BoardRepository boardRepository;
     private final ReplyRepository replyRepository;
 
-    public ReplyRepositoryTest(@Autowired ReplyRepository replyRepository) {
+    public ReplyRepositoryTest(
+            @Autowired BoardRepository boardRepository,
+            @Autowired ReplyRepository replyRepository) {
+        this.boardRepository = boardRepository;
         this.replyRepository = replyRepository;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        createBoardAndReply();
     }
 
     @DisplayName("insert 테스트")
     @Test
     public void insert() {
         //given
-        long totalCount = replyRepository.count();
-        final Long boardId = 1L;
-        final Long memberId = 1L;
+        final Long userId = 1L;
+        long boardTotalCount = boardRepository.count();
+        long replyTotalCount = replyRepository.count();
+        Board board = new Board();
+        board.setTitle("제목 테스트입니다.");
+        board.setContent("본문내용 테스트입니다.");
+        board.setCategory(Category.COMMON);
+        board.setStatus(Status.ACTIVE);
+        board.setMember_id(1L);
 
-        Reply reply = Reply.builder()
-                .id(1L)
+        boardRepository.save(board);
+
+        ReplyDto.ReplyRequestDto replyRequestDto = ReplyDto.ReplyRequestDto.builder()
+                .boardId(board.getId())
+                .memberId(board.getMember_id())
                 .content("댓글 테스트입니다.")
-                .boardId(boardId)
-                .memberId(memberId)
                 .build();
+
+        Reply reply = replyRequestDto.toEntity(board, null, userId);
 
         //when
         replyRepository.save(reply);
 
         //then
-        assertThat(replyRepository.count()).isEqualTo(totalCount + 1);
+        assertThat(boardRepository.count()).isEqualTo(boardTotalCount + 1);
+        assertThat(replyRepository.count()).isEqualTo(replyTotalCount + 1);
 
     }
 
@@ -47,24 +72,58 @@ class ReplyRepositoryTest {
     @Test
     public void delete() {
         //given
-        long totalCount = replyRepository.count();
-        final Long boardId = 1L;
-        final Long memberId = 1L;
+        final Long userId = 1L;
+        Board board = new Board();
+        board.setTitle("제목 테스트입니다.");
+        board.setContent("본문내용 테스트입니다.");
+        board.setCategory(Category.COMMON);
+        board.setStatus(Status.ACTIVE);
+        board.setMember_id(1L);
 
-        Reply reply = Reply.builder()
-                .id(1L)
+        boardRepository.save(board);
+
+        ReplyDto.ReplyRequestDto replyRequestDto = ReplyDto.ReplyRequestDto.builder()
+                .boardId(board.getId())
+                .memberId(board.getMember_id())
                 .content("댓글 테스트입니다.")
-                .boardId(boardId)
-                .memberId(memberId)
                 .build();
+
+        Reply reply = replyRequestDto.toEntity(board, null, userId);
 
         replyRepository.save(reply);
 
         //when
         replyRepository.deleteById(reply.getId());
-        
+        boardRepository.deleteById(board.getId());
+
         //then
+        assertThat(boardRepository.findAll()).doesNotContain(board);
         assertThat(replyRepository.findAll()).doesNotContain(reply);
-        
+
     }
+
+    private void createBoardAndReply() {
+        for (long i = 1; i <= 3; i++) {
+            Board board = new Board();
+            board.setTitle(i + "의 제목 입니다.");
+            board.setContent(i + "의 본문내용 입니다.");
+            board.setCategory(Category.COMMON);
+            board.setStatus(Status.ACTIVE);
+            board.setMember_id(1L);
+
+            boardRepository.save(board);
+
+            for (long j = 1; j <= 3; j++) {
+                ReplyDto.ReplyRequestDto replyRequestDto = ReplyDto.ReplyRequestDto.builder()
+                        .content(j + "번째 댓글입니다.")
+                        .boardId(j)
+                        .memberId(1L)
+                        .build();
+
+                Reply reply = replyRequestDto.toEntity(board, null, 1L);
+                replyRepository.save(reply);
+            }
+        }
+    }
+
 }
