232c972c2ea3a2cd50ade1320b3cf2ab5fadd932
khsrla9806 <kimhunsope@naver.com>
2023-07-14 18:43:02 +0900

    [#27] refactor: BoardController 가독성을 위한 상수화 작업 진행

--- src/main/java/com/board/board/type/Category.java
+++ src/main/java/com/board/board/type/Category.java
@@ -1,5 +1,13 @@
 package com.board.board.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Category {
-    COMMON, PRO
+    COMMON("[새싹 회원 게시판]"),
+    PRO("[우수 회원 게시판]");
+
+    private final String categoryTitle;
 }
