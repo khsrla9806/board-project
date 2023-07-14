534a19dd6d4df7c8c16509bc59afc8aa41568b8f
khsrla9806 <kimhunsope@naver.com>
2023-07-14 18:37:21 +0900

    [#27] feat: 게시글 작성 Service 메서드에 로그인된 Member 정보 추가
    - 작성자에 대한 Principal 객체를 받아서 email로 해당되는 Member가 존재하는지 확인
    - 존재한다면 Board와 Member의 연관관계를 설정

--- src/main/java/com/board/board/service/BoardService.java
+++ src/main/java/com/board/board/service/BoardService.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.security.Principal;
+
 public interface BoardService {
-    Long save(BoardDto.CreateRequest dto, MultipartFile thumbnail);
+    Long save(BoardDto.CreateRequest dto, MultipartFile thumbnail, Principal principal);
     Page<BoardDto.ListResponse> findAll(Pageable pageable, String keyword);
     Page<BoardDto.ListResponse> findAllByCategory(Category category, Pageable pageable, String keyword);
     BoardDto.DetailResponse findById(Long id);
