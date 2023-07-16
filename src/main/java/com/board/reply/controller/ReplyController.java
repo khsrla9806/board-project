d903e1bca7fff1599cd19da566da6082c6575a44
SYH <syh05247@naver.com>
2023-07-17 00:01:36 +0900

    [#28] refactor: ReplyController 리펙터링
    * Principal 객체를 받아서 service 단에 넘겨주는 형식으로 변경함
    * 댓글 삭제 URI 를 '/board/{boardId}/reply/delete/{replyId}' 로 변경하였음

--- src/main/java/com/board/reply/controller/ReplyController.java
+++ src/main/java/com/board/reply/controller/ReplyController.java
@@ -4,8 +4,11 @@ import com.board.reply.dto.ReplyDto;
 import com.board.reply.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/board")
@@ -14,27 +17,29 @@ public class ReplyController {
 
     private final ReplyService replyService;
 
-    // @GetMapping("/{boardId}")
-    public String ListBoardAndReply(@PathVariable Long boardId, ModelMap model) {
-        ReplyDto.BoardWithReplyDto boardWithReplies = replyService.findBoardWithReplies(boardId);
-
-        model.addAttribute("board", boardWithReplies);
-        model.addAttribute("replies", boardWithReplies.getReplyResponseDtos());
-
-        return "/replies/boardAndReply";
-
-    }
+    // MemberController 로 이동
+//    @GetMapping("/{boardId}")
+//    public String ListBoardAndReply(@PathVariable Long boardId, ModelMap model) {
+//        ReplyDto.BoardWithReplyDto boardWithReplies = replyService.findBoardWithReplies(boardId);
+//
+//        model.addAttribute("board", boardWithReplies);
+//        model.addAttribute("replies", boardWithReplies.getReplyResponseDtos());
+//
+//        return "/replies/boardAndReply";
+//
+//    }
 
     @PostMapping("/reply/new")
-    public String addReply(ReplyDto.ReplyRequestDto replyRequestDto) {
-        replyService.addReply(replyRequestDto);
+    public String addReply(ReplyDto.ReplyRequestDto replyRequestDto, Principal principal) {
+        replyService.addReply(replyRequestDto, principal);
 
         return "redirect:/board/" + replyRequestDto.getBoardId();
     }
 
-    @PostMapping("/reply/delete/{replyId}")
-    public String removeReply(@PathVariable Long replyId, Long boardId) {
-        replyService.removeReply(replyId);
+    @PostMapping("/{boardId}/reply/delete/{replyId}")
+    public String removeReply(@PathVariable Long boardId, @PathVariable Long replyId, Principal principal) {
+
+        replyService.removeReply(replyId, principal);
 
         return "redirect:/board/" + boardId;
     }
