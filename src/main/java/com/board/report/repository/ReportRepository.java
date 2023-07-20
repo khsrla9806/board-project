38bd4280f275464f173fbf53085117d5dc59447f
jyjang <jinyngg@gmail.com>
2023-07-20 19:48:55 +0900

    [#50] feat: 메인 모듈 src 삭제

--- src/main/java/com/board/report/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.board.report.repository;
-
-import com.board.report.domain.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
