b1e20caf9ebe858858fc7d23b2a3834da48debb8
khsrla9806 <kimhunsope@naver.com>
2023-07-21 17:43:07 +0900

    [#55] feat: 회원 권한 설정 Repository 메서드 추가 및 MyBatis SQL 작성

--- module-admin/src/main/java/com/module/adminMember/repository/AdminMemberRepository.java
+++ module-admin/src/main/java/com/module/adminMember/repository/AdminMemberRepository.java
@@ -1,11 +1,14 @@
 package com.module.adminMember.repository;
 
 import com.module.adminMember.dto.AdminMemberDto;
+import com.module.member.domain.Member;
+import com.module.member.type.MemberRole;
 import com.module.member.type.MemberStatus;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface AdminMemberRepository {
@@ -18,5 +21,9 @@ public interface AdminMemberRepository {
     );
 
     void updateMemberStatus(@Param("memberId") Long memberId, @Param("memberStatus") MemberStatus memberStatus);
+
+    Optional<Member> findMemberById(@Param("memberId") Long memberId);
+
+    void updateMemberById(@Param("memberId") Long memberId, @Param("memberRole") MemberRole memberRole);
 
 }
