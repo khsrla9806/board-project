e72f405597fa562d31e48c894bc4aef1c5ba343b
SYH <syh05247@naver.com>
2023-07-21 13:23:37 +0900

    [#56] feat: AdminReportController 추가
    * /admin/reports URI 를 요청하면 신고 목록을 model 에 담아서 /admin/report view 단에 전달한다.
    
    * 신고한 증거 이미지를 가져오기 위한 findEvidenceImage controller 구현

---
+++ module-admin/src/main/java/com/module/adminReport/controller/AdminReportController.java
@@ -0,0 +1,48 @@
+package com.module.adminReport.controller;
+
+import com.module.adminReport.dto.AdminReportDto;
+import com.module.adminReport.service.AdminReportService;
+import com.module.board.type.BoardView;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminReportController {
+
+    private final AdminReportService adminReportService;
+
+    @GetMapping("/reports")
+    public String reports(Model model) {
+        List<AdminReportDto> allReports = adminReportService.findAllReportWithMemberAndBoard();
+
+        model.addAttribute("response", allReports);
+        return "/admin/report";
+    }
+
+    @ResponseBody
+    @GetMapping("/report/image/{filename}")
+    public Resource findEvidenceImage(@PathVariable String filename) throws MalformedURLException {
+
+        String homeDir = System.getProperty("user.home");
+        String uploadFolderName = "board-store";
+
+        String fullPath = homeDir + File.separator + uploadFolderName + File.separator + filename;
+
+        return new UrlResource("file:" + fullPath);
+    }
+
+
+}
