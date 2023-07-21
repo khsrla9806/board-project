6d8ff69eddb312da940ecfa2a6263b1d6e8f0653
khsrla9806 <kimhunsope@naver.com>
2023-07-21 12:13:39 +0900

    [#20] refactor: BoardDto에 Builder 적용
    - Setter는 지양하는 편이 좋다고 해서 Builder를 적용

--- module-domain/src/main/java/com/module/board/dto/BoardDto.java
+++ module-domain/src/main/java/com/module/board/dto/BoardDto.java
@@ -5,10 +5,7 @@ import com.module.board.type.Category;
 import com.module.member.domain.Member;
 import com.module.reply.domain.Reply;
 import com.module.reply.dto.ReplyDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
@@ -18,7 +15,7 @@ import java.util.stream.Collectors;
 public class BoardDto {
 
     @Getter
-    @Setter
+    @Builder
     public static class CreateRequest {
         @NotBlank(message = "제목은 필수 입력 값입니다.")
         @Length(max = 255, message = "제목은 255자를 넘어갈 수 없습니다.")
@@ -30,7 +27,7 @@ public class BoardDto {
     }
 
     @Getter
-    @Setter
+    @Builder
     public static class UpdateRequest {
         private Long id;
         @NotBlank(message = "제목은 필수 입력 값입니다.")
@@ -43,7 +40,6 @@ public class BoardDto {
     }
 
     @Getter
-    @AllArgsConstructor
     @Builder
     public static class ListResponse {
         private Long id;
