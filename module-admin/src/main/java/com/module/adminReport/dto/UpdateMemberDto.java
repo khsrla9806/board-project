4df6c8fb0330699865a886139d380d4ea08d2caf
khsrla9806 <kimhunsope@naver.com>
2023-07-21 17:41:06 +0900

    [#55] refactor: JSON 데이터를 파싱해서 받는 Dto에 빈 생성자 추가
    - json 요청 시에 제대로 파싱되지 않는 오류가 있음 (맥에서만 존재하는데, 일단 추가)

--- module-admin/src/main/java/com/module/adminReport/dto/UpdateMemberDto.java
+++ module-admin/src/main/java/com/module/adminReport/dto/UpdateMemberDto.java
@@ -1,9 +1,13 @@
 package com.module.adminReport.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UpdateMemberDto {
 
