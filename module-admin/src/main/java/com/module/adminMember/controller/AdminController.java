a08f5ec1acbf24527a4dbe76243999714d4285ac
khsrla9806 <kimhunsope@naver.com>
2023-07-21 17:42:20 +0900

    [#55] feat: 회원 권한 설정 Controller 메서드 추가

--- module-admin/src/main/java/com/module/adminMember/controller/AdminController.java
+++ module-admin/src/main/java/com/module/adminMember/controller/AdminController.java
@@ -2,10 +2,12 @@ package com.module.adminMember.controller;
 
 import com.module.adminMember.dto.AdminMemberDto;
 import com.module.adminMember.service.AdminMemberService;
+import com.module.member.type.MemberRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,6 +26,23 @@ public class AdminController {
         model.addAttribute("response", response);
 
         return "/admin/members";
+    }
+
+    @GetMapping("/members-control")
+    public String memberControl(Model model) {
+
+        List<AdminMemberDto> response = memberService.findAllMemberWithBoardCountAndReplyCount();
+        model.addAttribute("response", response);
+        model.addAttribute("memberRoles", MemberRole.values());
+
+        return "/admin/members-control";
+    }
+
+    @PostMapping("/member-role")
+    public String updateRole(Long memberId, MemberRole memberRole) {
+        memberService.updateMemberRole(memberId, memberRole);
+
+        return "redirect:/admin/members-control";
     }
 
 }
