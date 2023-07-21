232113e496a2ee0585bc1b4608b30fa7f8f08abe
jyjang <jinyngg@gmail.com>
2023-07-21 15:43:37 +0900

    [#49] feat: 이메일 전송 기능 버그 수정 및 리팩토링
    1. 테이블 저장 관련 수정
    2. 리팩토링

--- module-admin/src/main/java/com/module/admin/transfer/controller/EmailController.java
+++ module-admin/src/main/java/com/module/admin/transfer/controller/EmailController.java
@@ -4,9 +4,9 @@ import com.module.admin.transfer.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin/email")
@@ -16,9 +16,21 @@ public class EmailController {
     private final EmailService emailService;
 
     @GetMapping("/write/{receiverId}")
-    public String showEmailForm(Model model, @PathVariable Long receiverId) {
+    public String showEmailForm(Model model, @PathVariable String receiverId) {
         model.addAttribute("receiverId", receiverId);
         return "admin/create-email-form";
+    }
+
+    @PostMapping("/send/{receiverId}")
+    public String sendEmail(
+            Principal principal
+            , @PathVariable("receiverId") String receiverId
+            , @RequestParam("title") String title
+            , @RequestParam("content") String content
+    ) {
+        String senderEmail = principal.getName();
+        emailService.sendAndSaveEmail(title, content, senderEmail, Long.valueOf(receiverId));
+        return "redirect:/admin/members";
     }
 
 }
