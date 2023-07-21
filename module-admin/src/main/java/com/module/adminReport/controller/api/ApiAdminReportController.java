8f42b5c6b58be8d0ff30042e26f730ac5017762b
SYH <syh05247@naver.com>
2023-07-21 13:26:02 +0900

    [#56] feat: ApiAdminReportController 추가
    * reports 메서드는 /api/admin/reports 쿼리파라미터로 신고 사유(reason) 를 요청하면 필터링 된 신고 목록을 보여준다.
    
    * updateMemberStatus 는 유저의 상태 값 (MemberStatus)를 변경하고 그에 따라 ResponseEntity 를 return 한다.

---
+++ module-admin/src/main/java/com/module/adminReport/controller/api/ApiAdminReportController.java
@@ -0,0 +1,46 @@
+package com.module.adminReport.controller.api;
+
+import com.module.adminReport.dto.AdminReportDto;
+import com.module.adminReport.dto.UpdateMemberDto;
+import com.module.adminReport.service.AdminReportService;
+import com.module.member.type.MemberStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class ApiAdminReportController {
+
+    private final AdminReportService adminReportService;
+
+    @GetMapping("/reports")
+    public ResponseEntity<List<AdminReportDto>> reports(@RequestParam(value = "reason", defaultValue = "") String reason) {
+
+        List<AdminReportDto> response = adminReportService.findAllReportWithMemberAndBoardByReason(reason);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/report/member/update")
+    public ResponseEntity<String> updateMemberStatus(@RequestBody UpdateMemberDto updateMemberDto) {
+
+        try {
+            MemberStatus memberStatus = MemberStatus.valueOf(updateMemberDto.getMemberStatus());
+            adminReportService.updateMemberStatus(updateMemberDto.getMemberId(), memberStatus);
+            return ResponseEntity.ok("회원 상태가 변경되었습니다.");
+        } catch (IllegalArgumentException e) {
+            // Enum 값 파싱에 실패한 경우 (잘못된 상태 값이 들어온 경우)
+            return ResponseEntity.badRequest().body("잘못된 회원 상태 값입니다.");
+        } catch (Exception e) {
+            // 다른 예외가 발생한 경우 (예를 들어 데이터베이스 오류 등)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
+        }
+    }
+
+}
