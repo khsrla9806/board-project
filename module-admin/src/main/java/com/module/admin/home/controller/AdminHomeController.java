f4b9904df81b6ec809c0fbbfef6696558e746c56
jyjang <jinyngg@gmail.com>
2023-07-21 17:49:59 +0900

    [#58] refactor: 회원 HomeController 구성

---
+++ module-admin/src/main/java/com/module/admin/home/controller/AdminHomeController.java
@@ -0,0 +1,25 @@
+package com.module.admin.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/")
+public class AdminHomeController {
+
+    @RequestMapping
+    public String home(Principal principal) {
+        if (validatePrincipal(principal)) {
+            return "redirect:/members/login";
+        }
+
+        return "redirect:/admin/members";
+    }
+
+    /** 로그인된 회원 검증 */
+    private boolean validatePrincipal(Principal principal) {
+        return principal == null;
+    }
+}
